21d1357f7a4a291102a7c5a5216d2730d01df4e1
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-09-23 13:07:57 +0300

    email component complete

--- app/components/mail/index.jsx
+++ app/components/mail/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
+import BemyButton from '../button';
 import styles from './main.scss';
 
 const MailComponent = props => {
@@ -7,30 +8,31 @@ const MailComponent = props => {
   return (
     <section className={styles.mail}>
       <header>
-        <svg width="16px" height="16px" viewBox="0 0 16 16" onClick={() => browserHistory.push('/people')}>
-          <g id="App" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-            <g id="Profile" transform="translate(-22.000000, -44.000000)">
-              <g id="ic_arrow_back_24px" transform="translate(18.000000, 40.000000)">
-                <polygon id="Shape" points="0 0 24 0 24 24 0 24" />
-                <polygon className={styles.arrow} id="Shape" fill="#9B9B9B" points="20 11 7.83 11 13.42 5.41 12 4 4 12 12 20 13.41 18.59 7.83 13 20 13" />
+        <button onClick={() => browserHistory.push('/people')}>
+          <svg width="20px" height="20px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g id="App-2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+              <g id="Atoms" transform="translate(-108.000000, -2360.000000)">
+                <g id="ic_close" transform="translate(102.000000, 2354.000000)">
+                  <polygon id="Shape" fill="#C2C2C2" points="25.3333333 8.54666667 23.4533333 6.66666667 16 14.12 8.54666667 6.66666667 6.66666667 8.54666667 14.12 16 6.66666667 23.4533333 8.54666667 25.3333333 16 17.88 23.4533333 25.3333333 25.3333333 23.4533333 17.88 16" />
+                  <polygon id="Shape" points="0 0 32 0 32 32 0 32" />
+                </g>
               </g>
             </g>
-          </g>
-        </svg>
-        <figure>
+          </svg>
+        </button>
+        <h3>Сообщение</h3>
+        {/* <figure>
           <img src={human.photo} alt={human.photo} />
           <figcaption>
             <h3>{human.name}</h3>
           </figcaption>
-        </figure>
+        </figure> */}
       </header>
       <main>
         <div>
           <textarea placeholder={human.gender === 'female' ? 'Напишите  пару слов о себе и что вам понравилось в девушке' : 'Напишите  пару слов о себе и что вам понравилось в парне'}  />
         </div>
-        <button onClick={() => props.harmonyActions.sendMail(human.id)}>
-          ОТПРАВИТЬ
-        </button>
+        <BemyButton enabled to="/profile" title="Отправить сейчас" />
         <span>ответ вам прийдет на {props.facebook.email }</span>
       </main>
     </section>
